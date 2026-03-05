7f2a9ca00b1b3279c06ffb2b7c57a31b10346909
sbx_user1051 <sbx_user1051@169.254.35.117>
2026-03-05 13:32:03 +0000

    Thu Mar 05 13:32:03 UTC 2026

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,12 @@
 
 class SumSolution:
-    
-    def compute(self, x, y):
-        raise NotImplementedError()
+
+    def compute(self, x: int, y: int) -> int:
+        """
+        sum(integer, integer) -> integer
+        - param[0] = a positive integer between 0-100
+        - param[1] = a positive integer between 0-100
+        - @return = an integer representing the sum of the two numbers
+        """
+        return x + y
+
